da5e388fbb144981316cb77d2dfceed1e8e46cbb
Allan Freitas <allan.cassio1@gmail.com>
2021-01-17 10:46:04 -0300

    started launch-store

---
+++ src/app/lib/utils.js
@@ -0,0 +1,14 @@
+module.exports = {
+  date: timestemp => {
+    const date = new Date(timestemp)
+    const year = date.getUTCFullYear()
+    const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+    const day = `0${date.getUTCDate()}`.slice(-2)
+    return {
+      day,
+      month,
+      year,
+      iso: `${year}-${month}-${day}`
+    }
+  }
+}
