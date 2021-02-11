35adac8d49981c6b2b0ab5c2cb11bc06c847f87a
Allan Freitas <allan.cassio1@gmail.com>
2021-02-10 21:23:44 -0300

    ajustando rotas e query do banco de dados.

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const routes = express.Router()
+const ProductsController = require('./app/controllers/ProductsController')
 
 routes.get('/', function (req, res) {
   return res.render('layout')
 })
-
+routes.get('/products/create', ProductsController.create)
+routes.post('/products', ProductsController.post)
+routes.get('/ads/create', function (req, res) {
+  return res.redirect("/products/create")
+})
 
 
 module.exports = routes
