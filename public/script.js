59ef42bdc04805b5bdf1d7871660801877886dff
Allan Freitas <allan.cassio1@gmail.com>
2021-04-15 08:17:15 -0300

    previews from image and alter when click

--- public/script.js
+++ public/script.js
@@ -100,4 +100,20 @@ const PhotosUpload = {
     }
     photoDiv.remove()
   }
+
+}
+
+const ImageGallery = {
+  previews: document.querySelectorAll('.gallery-preview img'),
+  highlight: document.querySelector('.gallery .highlight > img'),
+  setImage(e) {
+    const {target} = e
+
+
+    ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+
+    target.classList.add('active')
+    
+    ImageGallery.highlight.src = target.src
+  }
 }
