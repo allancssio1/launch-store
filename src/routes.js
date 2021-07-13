19b84912897ec47b71d9b43fba046ce6eb13ac58
Allan Freitas <allan.cassio1@gmail.com>
2021-07-13 18:45:53 -0300

    created routes for search of products

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const routes = express.Router()
 const multer = require('./app/middlewares/multer')
+
 const ProductsController = require('./app/controllers/ProductsController')
 const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
 routes.get('/', HomeController.index)
+
+routes.get('/products/search', SearchController.index)
+
 routes.get('/products/create', ProductsController.create)
 routes.get('/products/:id/edit', ProductsController.edit)
 routes.get('/products/:id', ProductsController.show)
+
 routes.post('/products', multer.array("photos", 6), ProductsController.post)
 routes.put('/products', multer.array('photos', 6),ProductsController.put)
 routes.delete('/products', ProductsController.delete)
@@ -15,6 +21,5 @@ routes.delete('/products', ProductsController.delete)
 routes.get('/ads/create', function (req, res) {
   return res.redirect("/products/create")
 })
-
 
 module.exports = routes
