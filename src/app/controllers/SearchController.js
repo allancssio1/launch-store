d4d57b92b9b91408e955aa7ba7971d4929656f51
Allan Freitas <allan.cassio1@gmail.com>
2021-07-14 17:38:01 -0300

    removed total_query for not using

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -19,7 +19,27 @@ module.exports = {
 
       results = await Product.search(params)
 
+      async function getImage(productId) {
+        let results = await Product.files(productId)
+        const files = results.rows.map (file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
 
+        return files[0]
+      }
+
+      const productsPormise = results.rows.map(async product => {
+        product.img = await getImage(product.id)
+        product.oldPrice = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+
+        return product
+      })
+
+      const products = await Promise.all(productsPormise)
+
+      const search = {
+        term: req.query.fileter,
+        total: products.length
+      }
 
       return res.render("search/index", { products })
       
