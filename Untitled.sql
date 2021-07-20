6cf8bfdf4957c4e21411eff5d5afd9395b4f5551
Allan Freitas <allan.cassio1@gmail.com>
2021-07-19 21:07:45 -0300

    adjusted untitled for db

--- Untitled.sql
+++ Untitled.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS launchstore;
+CREATE DATABASE launchstore;
+
 CREATE TABLE "products" (
   "id" SERIAL PRIMARY KEY,
   "category_id" int NOT NULL,
@@ -27,3 +30,44 @@ CREATE TABLE "files" (
 ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 
 ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "name" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "cpf_cnpj" int UNIQUE NOT NULL,
+  "cep" text,
+  "address" text,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+-- FOREYGN KEY DE USUÁRIO COM OS PRODUTOS
+ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+-- CRIAÇÃO DE TRIGGER
+CREATE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+  BEGIN
+    NEW.updated.at = NOW();
+    RETURN NEW
+  END;
+$$ LANGUAGE plpgsql;
+
+-- PROCEDURES PARA AUTO ATUALIZAÇÃO DO UPDATED_AT DE PRODUTOS
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- PROCEDURES PARA AUTO ATUALIZAÇÃO DO UPDATED_AT DE PRODUTOS
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- CATEGORIAS PRE CADASTRADAS
+INSERT INTO  categories(name) VALUES ('Alimento');
+INSERT INTO  categories(name) VALUES ('Eletrônicos');
+INSERT INTO  categories(name) VALUES ('Automoveis');
