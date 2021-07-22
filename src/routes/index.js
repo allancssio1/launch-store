aae7374b2a58b2d484754e35b0959b778a0a6e7a
Allan Freitas <allan.cassio1@gmail.com>
2021-07-22 16:12:12 -0300

    conclued config routes for start of register

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,12 @@ const routes = express.Router()
 
 const HomeController = require('../app/controllers/HomeController')
 
-// const users = require('./users')
 const products = require('./products')
+const users = require('./users')
 
 routes.get('/', HomeController.index)
 
-// routes.use('/users', users)
+routes.use('/users', users)
 routes.use('/products', products)
 
 routes.get('/ads/create', function (req, res) {
