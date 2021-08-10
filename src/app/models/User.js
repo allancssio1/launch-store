92b9f4cec0b28c2435c1c2101e80def24e427c94
Allan Freitas <allan.cassio1@gmail.com>
2021-08-10 08:10:28 -0300

    create models for filter user in database.

---
+++ src/app/models/User.js
@@ -0,0 +1,26 @@
+const db = require("../../config/db")
+
+module.exports = {
+  async findOne(filters) {
+    let query = `SELECT * FROM users` //inicio da estrura de select
+
+    // para cada uma das posições do filter crie uma chave
+    Object.keys(filters).map (key => {
+      query = `${query} ${key}` // completa o select com a chave key === where
+
+      // dentro cada filter na posição da key faça um map
+      Object.keys(filters[key]).map(field => {
+        /* pega a query ja montada e acrescenta o dado enviado
+        * dentro do filters na posilão dessa key pegando o dado field
+        * e coloque dentro dessa nova query
+        */
+        query = `${query} ${field} = '${filters[key][field]}'` 
+      })
+
+      const results = await db.query(query)
+
+      return results.rows[0]
+    })
+
+  }
+}
