39537f8e44f6c7520ccefb3bc2e95fe53c1ffec0
Allan Freitas <allan.cassio1@gmail.com>
2021-09-23 10:26:56 -0300

    update for search error on code

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,11 +1,27 @@
 const User = require('../models/User')
+const {formatCpfCnpj, formatCep} = require('../lib/utils')
 
 module.exports = {
   registerForm (req, res) {
     return res.render("user/register")
   },
-  show(req, res) {
-    return res.send('cadastrado')
+  async show(req, res) {
+    const {userId: id} = req.session
+
+    const user = await User.findOne({
+      where: {id},
+    })
+    console.log("aqui ==>", user)
+
+    if(!user) return res.render("user/register", {
+      error: 'Usuário não encontrado!'
+    })
+    console.log("aqui ==>!!", user)
+
+
+    // user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
+    // user.cep = formatCep(user.cep)
+    return res.render('user/index', {user})
   }
   ,
   async post(req, res) {
--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -21,5 +21,43 @@ module.exports = {
       style: 'currency',
       currency: 'BRL'
     }).format(price/100)
+  },
+  formatCep(value) {
+    value = value.replace(/\D/g, "")
+    
+    if (value.length > 8)
+      value = value.slice(0, -1)
+
+    value = value.replace(/(\d{5})(\d)/, "$1-$2")
+
+    return value
+  },
+  formatCpfCnpj(value) {
+    value = value.replace(/\D/g, "")
+
+    if(value.length > 14) {
+      value = value.slice(0, -1)
+    }
+
+    /* chef if cnpj => 99.999.999/0001-99
+    * else is cpf => 999.999.999-99
+    * (\d{repete x vezes}), "valor"
+    */
+    if(value.length > 11) {
+
+      value = value.replace(/(\d{2})(\d)/, "$1.$2")
+      value = value.replace(/(\d{3})(\d)/, "$1.$2")
+      value = value.replace(/(\d{3})(\d)/, "$1/$2")
+      value = value.replace(/(\d{4})(\d)/, "$1-$2")
+
+    } else {
+
+      value = value.replace(/(\d{3})(\d)/, "$1.$2")
+      value = value.replace(/(\d{3})(\d)/, "$1.$2")
+      value = value.replace(/(\d{3})(\d)/, "$1-$2")
+
+    }
+
+    return value
   }
 }
