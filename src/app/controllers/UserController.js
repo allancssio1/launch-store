8f2f68c7a9f91c665bd4a6e22860aade916ca9bd
Allan Freitas <allan.cassio1@gmail.com>
2021-10-14 21:15:19 -0300

    create function from update on user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,13 +6,7 @@ module.exports = {
     return res.render("user/register")
   },
   async show(req, res) {
-    const {userId: id} = req.session
-
-    const user = await User.findOne({ where: {id} })
-
-    if(!user) return res.render("user/register", {
-      error: 'Usuário não encontrado!'
-    })
+    const {user} = req
 
     user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
     user.cep = formatCep(user.cep)
@@ -27,5 +21,18 @@ module.exports = {
 
     return res.redirect('/users')
 
+  },
+  async update(req, res) {
+    try {
+      
+    } catch (error) {
+      console.error(err)
+
+      return res.render('user/index', {
+        error: 'error'
+      })
+      
+    }
+    return res.send("aqui deu")
   }
 }
--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,40 +1,96 @@
 const User = require("../models/User")
+const { compare } = require('bcryptjs')
+
+function checkAllFields(body) {
+  const keys = Object.keys(body.body)
+
+  for (key of keys) {
+    if (body.body[key] === "")
+      return {
+        user: body.body,
+        error: 'Preencha todos os dados.'
+      }
+  }
+  return body
+}
+
+async function show (req, res, next) {
+  const {userId: id} = req.session
+
+  const user = await User.findOne({ where: {id} })
+
+  if(!user) return res.render("user/register", {
+    error: 'Usuário não encontrado!'
+  }) 
+  
+  req.user = user
+
+  next()
+}
 
 async function post(req, res, next) {
-  const keys = Object.keys(req.body)
+  
+  const fillAllFields = checkAllFields(req.body)
 
-    for (key of keys) {
-      if (req.body[key] === "")
-        return res.render("user/register", {
-          user: req.body,
-          error: 'Preencha todos os dados.'
-        })
-    }
+  if(fillAllFields) {
+    return res.render('user/register', fillAllFields)
+  }
 
-    let { email, cpf_cnpj, password, passwordRepeat } = req.body
+  let { email, cpf_cnpj, password, passwordRepeat } = req.body
 
-    cpf_cnpj = cpf_cnpj.replace(/\D/g, '')
-    
-    const user = await User.findOne({
-      WHERE: { email },
-      OR: { cpf_cnpj }
-    })
+  cpf_cnpj = cpf_cnpj.replace(/\D/g, '')
+  
+  const user = await User.findOne({
+    WHERE: { email },
+    OR: { cpf_cnpj }
+  })
 
-    if(user) return res.render("user/register", {
+  if(user) return res.render("user/register", {
+    user: req.body,
+    error: 'Usuário já existe.'
+  })
+
+  if (password !== passwordRepeat) {
+    return res.render("user/register", {
       user: req.body,
-      error: 'Usuário já existe.'
+      error: 'Senha não são indênticas.'
     })
+  }
 
-    if (password !== passwordRepeat) {
-      return res.render("user/register", {
-        user: req.body,
-        error: 'Senha não são indênticas.'
-      })
-    }
+  next()
+}
+async function update(req, res, next) {
+  // verificação se campos preenchidos
+  const fillAllFields = checkAllFields(req.body)
+  if(fillAllFields) {
+    return res.render('user/register', fillAllFields)
+  }
 
-    next()
+  const { id, password } = req.body
+
+  //campo de senha vazio
+  if (!password) return req.render('user/index', {
+    user: req.body,
+    error: 'Coloque sua senha para atualizar seu cadastro'
+  })
+
+  const user = await User.findOne({where: {id}})
+  // compare é uma função do bcryptjs
+  const passed = await compare(password, user.password)
+
+  //se senhas criptografadas forem diferentes.
+  if (!passed) return res.render('user/index', {
+    user: req.body,
+    error: 'Senha incorreta.'
+  })
+
+  req.user = user
+
+  next()
 }
 
 module.exports = {
-  post
+  post,
+  show,
+  update
 }
